b5992d788befb27021355468bb8650fadbde751c
wangtingwei <wang_tingwei@126.com>
2018-08-10 21:39:33 +0800

    test

--- net.java
+++ net.java
@@ -1,2 +1,26 @@
-wangtingwei
-tkjest
+eno1: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
+        ether 00:1f:c6:9c:d7:ba  txqueuelen 1000  (Ethernet)
+        RX packets 0  bytes 0 (0.0 B)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 0  bytes 0 (0.0 B)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+        device interrupt 16  memory 0xdc300000-dc320000  
+
+lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
+        inet 127.0.0.1  netmask 255.0.0.0
+        inet6 ::1  prefixlen 128  scopeid 0x10<host>
+        loop  txqueuelen 1000  (Local Loopback)
+        RX packets 205  bytes 15026 (15.0 KB)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 205  bytes 15026 (15.0 KB)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+wlp3s0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+        inet 172.16.16.88  netmask 255.255.240.0  broadcast 172.16.31.255
+        inet6 fe80::a3e2:a4b:6403:cf43  prefixlen 64  scopeid 0x20<link>
+        ether ac:ed:5c:c4:3e:70  txqueuelen 1000  (Ethernet)
+        RX packets 10894  bytes 9479572 (9.4 MB)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 4395  bytes 595370 (595.3 KB)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
