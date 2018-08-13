46e074f624bcee5ef1d563394f689074c37181f5
wangtingwei <wang_tingwei@126.com>
2018-08-13 22:08:02 +0800

    push-ifconfig

--- net.java
+++ net.java
@@ -1,19 +1,3 @@
-br-784398c8bc62: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
-        inet 172.19.0.1  netmask 255.255.0.0  broadcast 172.19.255.255
-        ether 02:42:c5:19:70:4b  txqueuelen 0  (Ethernet)
-        RX packets 0  bytes 0 (0.0 B)
-        RX errors 0  dropped 0  overruns 0  frame 0
-        TX packets 0  bytes 0 (0.0 B)
-        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
-
-docker0: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
-        inet 172.17.0.1  netmask 255.255.0.0  broadcast 172.17.255.255
-        ether 02:42:73:cc:d9:80  txqueuelen 0  (Ethernet)
-        RX packets 0  bytes 0 (0.0 B)
-        RX errors 0  dropped 0  overruns 0  frame 0
-        TX packets 0  bytes 0 (0.0 B)
-        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
-
 eno1: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
         ether 00:1f:c6:9c:d7:ba  txqueuelen 1000  (Ethernet)
         RX packets 0  bytes 0 (0.0 B)
@@ -26,18 +10,18 @@ lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
         inet 127.0.0.1  netmask 255.0.0.0
         inet6 ::1  prefixlen 128  scopeid 0x10<host>
         loop  txqueuelen 1000  (Local Loopback)
-        RX packets 665  bytes 56968 (56.9 KB)
+        RX packets 112  bytes 7860 (7.8 KB)
         RX errors 0  dropped 0  overruns 0  frame 0
-        TX packets 665  bytes 56968 (56.9 KB)
+        TX packets 112  bytes 7860 (7.8 KB)
         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
 
 wlp3s0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
         inet 172.16.16.88  netmask 255.255.240.0  broadcast 172.16.31.255
         inet6 fe80::a3e2:a4b:6403:cf43  prefixlen 64  scopeid 0x20<link>
         ether ac:ed:5c:c4:3e:70  txqueuelen 1000  (Ethernet)
-        RX packets 5073  bytes 2671045 (2.6 MB)
+        RX packets 32  bytes 8431 (8.4 KB)
         RX errors 0  dropped 0  overruns 0  frame 0
-        TX packets 4260  bytes 830943 (830.9 KB)
+        TX packets 52  bytes 7415 (7.4 KB)
         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
 
-2018年 08月 13日 星期一 21:50:28 CST
+2018年 08月 13日 星期一 22:08:02 CST
