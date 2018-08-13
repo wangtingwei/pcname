4f8106bacc65752e288e0f71ff0bf19bd1ea988c
wangtingwei <wang_tingwei@126.com>
2018-08-13 21:45:56 +0800

    push-ifconfig

--- net.java
+++ net.java
@@ -1,6 +1,6 @@
 br-784398c8bc62: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
         inet 172.19.0.1  netmask 255.255.0.0  broadcast 172.19.255.255
-        ether 02:42:42:8e:b8:60  txqueuelen 0  (Ethernet)
+        ether 02:42:c5:19:70:4b  txqueuelen 0  (Ethernet)
         RX packets 0  bytes 0 (0.0 B)
         RX errors 0  dropped 0  overruns 0  frame 0
         TX packets 0  bytes 0 (0.0 B)
@@ -8,7 +8,7 @@ br-784398c8bc62: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
 
 docker0: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
         inet 172.17.0.1  netmask 255.255.0.0  broadcast 172.17.255.255
-        ether 02:42:87:e4:84:f4  txqueuelen 0  (Ethernet)
+        ether 02:42:73:cc:d9:80  txqueuelen 0  (Ethernet)
         RX packets 0  bytes 0 (0.0 B)
         RX errors 0  dropped 0  overruns 0  frame 0
         TX packets 0  bytes 0 (0.0 B)
@@ -26,18 +26,18 @@ lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
         inet 127.0.0.1  netmask 255.0.0.0
         inet6 ::1  prefixlen 128  scopeid 0x10<host>
         loop  txqueuelen 1000  (Local Loopback)
-        RX packets 180  bytes 12677 (12.6 KB)
+        RX packets 620  bytes 52883 (52.8 KB)
         RX errors 0  dropped 0  overruns 0  frame 0
-        TX packets 180  bytes 12677 (12.6 KB)
+        TX packets 620  bytes 52883 (52.8 KB)
         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
 
 wlp3s0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
         inet 172.16.16.88  netmask 255.255.240.0  broadcast 172.16.31.255
         inet6 fe80::a3e2:a4b:6403:cf43  prefixlen 64  scopeid 0x20<link>
         ether ac:ed:5c:c4:3e:70  txqueuelen 1000  (Ethernet)
-        RX packets 303  bytes 250291 (250.2 KB)
+        RX packets 4628  bytes 2610135 (2.6 MB)
         RX errors 0  dropped 0  overruns 0  frame 0
-        TX packets 238  bytes 38529 (38.5 KB)
+        TX packets 3929  bytes 783609 (783.6 KB)
         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
 
-2018年 08月 13日 星期一 21:23:36 CST
+2018年 08月 13日 星期一 21:45:56 CST
