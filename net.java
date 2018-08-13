dbfd523f998f4f6a6fdfdb86e24eca451ebb0a8c
wangtingwei <wang_tingwei@126.com>
2018-08-13 20:49:10 +0800

    push-ifconfig

--- net.java
+++ net.java
@@ -1,1 +1,43 @@
-2018年 08月 13日 星期一 20:45:55 CST
+br-784398c8bc62: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
+        inet 172.19.0.1  netmask 255.255.0.0  broadcast 172.19.255.255
+        ether 02:42:13:07:24:c3  txqueuelen 0  (Ethernet)
+        RX packets 0  bytes 0 (0.0 B)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 0  bytes 0 (0.0 B)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+docker0: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
+        inet 172.17.0.1  netmask 255.255.0.0  broadcast 172.17.255.255
+        ether 02:42:3e:ab:5a:19  txqueuelen 0  (Ethernet)
+        RX packets 0  bytes 0 (0.0 B)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 0  bytes 0 (0.0 B)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+eno1: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
+        ether 00:1f:c6:9c:d7:ba  txqueuelen 1000  (Ethernet)
+        RX packets 0  bytes 0 (0.0 B)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 0  bytes 0 (0.0 B)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+        device interrupt 16  memory 0xdc300000-dc320000  
+
+lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
+        inet 127.0.0.1  netmask 255.0.0.0
+        inet6 ::1  prefixlen 128  scopeid 0x10<host>
+        loop  txqueuelen 1000  (Local Loopback)
+        RX packets 181  bytes 13784 (13.7 KB)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 181  bytes 13784 (13.7 KB)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+wlp3s0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+        inet 172.16.16.88  netmask 255.255.240.0  broadcast 172.16.31.255
+        inet6 fe80::a3e2:a4b:6403:cf43  prefixlen 64  scopeid 0x20<link>
+        ether ac:ed:5c:c4:3e:70  txqueuelen 1000  (Ethernet)
+        RX packets 1079  bytes 711958 (711.9 KB)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 297  bytes 45562 (45.5 KB)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+2018年 08月 13日 星期一 20:49:10 CST
