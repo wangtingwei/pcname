502b7e9b3907baf95f01f05045f3d8f1c8a4ed38
wangtingwei <wang_tingwei@126.com>
2018-08-29 10:53:37 +0800

    push-ifconfig

--- net.java
+++ net.java
@@ -10,18 +10,18 @@ lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
         inet 127.0.0.1  netmask 255.0.0.0
         inet6 ::1  prefixlen 128  scopeid 0x10<host>
         loop  txqueuelen 1000  (Local Loopback)
-        RX packets 113  bytes 7991 (7.9 KB)
+        RX packets 105  bytes 7461 (7.4 KB)
         RX errors 0  dropped 0  overruns 0  frame 0
-        TX packets 113  bytes 7991 (7.9 KB)
+        TX packets 105  bytes 7461 (7.4 KB)
         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
 
 wlp3s0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
         inet 172.16.16.204  netmask 255.255.240.0  broadcast 172.16.31.255
         inet6 fe80::a3e2:a4b:6403:cf43  prefixlen 64  scopeid 0x20<link>
         ether ac:ed:5c:c4:3e:70  txqueuelen 1000  (Ethernet)
-        RX packets 10  bytes 1428 (1.4 KB)
+        RX packets 7  bytes 1161 (1.1 KB)
         RX errors 0  dropped 0  overruns 0  frame 0
-        TX packets 10  bytes 1431 (1.4 KB)
+        TX packets 8  bytes 1198 (1.1 KB)
         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
 
-2018年 08月 28日 星期二 08:56:57 CST
+2018年 08月 29日 星期三 10:53:37 CST
