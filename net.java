6916c2b3202beec4f24743b72c9304acdfe2a337
wangtingwei <wang_tingwei@126.com>
2018-11-20 10:01:01 +0800

    push-ifconfig

--- net.java
+++ net.java
@@ -4,24 +4,24 @@ eno1: flags=4099<UP,BROADCAST,MULTICAST>  mtu 1500
         RX errors 0  dropped 0  overruns 0  frame 0
         TX packets 0  bytes 0 (0.0 B)
         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
-        device interrupt 16  memory 0xdc300000-dc320000  
+        device interrupt 16  memory 0xdc400000-dc420000  
 
 lo: flags=73<UP,LOOPBACK,RUNNING>  mtu 65536
         inet 127.0.0.1  netmask 255.0.0.0
         inet6 ::1  prefixlen 128  scopeid 0x10<host>
         loop  txqueuelen 1000  (Local Loopback)
-        RX packets 113  bytes 7855 (7.8 KB)
+        RX packets 129  bytes 9967 (9.9 KB)
         RX errors 0  dropped 0  overruns 0  frame 0
-        TX packets 113  bytes 7855 (7.8 KB)
+        TX packets 129  bytes 9967 (9.9 KB)
         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
 
 wlp3s0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
         inet 192.168.1.17  netmask 255.255.255.0  broadcast 192.168.1.255
         inet6 fe80::b9bb:99d1:1747:906d  prefixlen 64  scopeid 0x20<link>
         ether ac:ed:5c:c4:3e:70  txqueuelen 1000  (Ethernet)
-        RX packets 6  bytes 1231 (1.2 KB)
+        RX packets 14  bytes 2578 (2.5 KB)
         RX errors 0  dropped 0  overruns 0  frame 0
-        TX packets 15  bytes 2215 (2.2 KB)
+        TX packets 40  bytes 6258 (6.2 KB)
         TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
 
-2018年 11月 13日 星期二 16:57:38 CST
+2018年 11月 20日 星期二 10:01:01 CST
